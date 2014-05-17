f89cc724c1ed6b63862ee8d440091cd97671bffc
gougan <k_dot16@yahoo.co.jp>
2014-05-17 14:01:38 +0900

    no message

---
+++ a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("Content-type: text/html\n\n");
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<title>‹ã‹ã</title>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+	printf("\t");
+	for (int i=1;i<10;i++){
+		printf("%d\t",i);
+	}
+	printf("\n");
+	for (int i=1;i<10;i++){
+		printf("%d\t",i);
+		for(int j=1;j<10;j++){
+			printf("%d\t",j);
+		}
+		printf("\n");
+	}
+    printf("</body>\n");
+    printf("</html>\n");
+
+	return 0;
+
+}
+
